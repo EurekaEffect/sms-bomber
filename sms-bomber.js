63e9b79ebce9b8c71b20b9e4b892dce41c8a7f1b
EurekaEffect <eurekaeffecto@gmail.com>
2025-05-05 21:07:31 +0300

    Перший тест

--- sms-bomber.js
+++ sms-bomber.js
@@ -1,9 +1,53 @@
-import readline from 'readline'
+import readline from 'node:readline'
+import fs       from 'node:fs'
 
 (async () => {
-    const text = await ask('input:')
-    console.log(text)
+    const phone_number = await ask('Введіть номер телефону: +380')
+
+    const services_str = await readFile('./services.json')
+    const formatted_services_str = applyPhoneNumber(services_str, phone_number)
+
+    const services = JSON.parse(formatted_services_str);
+
+    for (let service of services) {
+        const apis = service['apis']
+
+        for (let api of apis) {
+            const { url, method } = api
+
+            const body = JSON.stringify(api['body'])
+            const expected_response = JSON.stringify(api['expected_response'])
+
+            await fetch(url, {
+                'method': method,
+                'body': body
+            }).then(async (res) => {
+                const json = await res.json()
+                const response = JSON.stringify(json)
+
+                console.log(url)
+                console.log(response === expected_response)
+            })
+        }
+    }
 })()
+
+function applyPhoneNumber(str_json, phone_number) {
+    return str_json.replaceAll('{phone_number}', phone_number)
+}
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (error, data) => {
+            if (error) {
+                reject(error)
+                return
+            }
+
+            resolve(data)
+        })
+    })
+}
 
 function ask(question) {
     const io = readline.createInterface({
